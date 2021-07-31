d7ae02351626bb5db054d58237fc2f56bbb28168
Callisto <edhirata@ucsc.edu>
2021-07-31 14:52:13 -0700

    early server stages

---
+++ basler_candidate/src/baslercam/server_thread.h
@@ -0,0 +1,25 @@
+
+#ifndef SERVER_THREAD_H
+#define SERVER_THREAD_H
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+void* server_thread(void* server_data);
+
+#endif
